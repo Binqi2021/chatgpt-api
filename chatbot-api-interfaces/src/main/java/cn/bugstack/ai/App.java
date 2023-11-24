53e4865904e30af822d1b77f29037cce5d00b755
Binqi2021 <87693565+Binqi2021@users.noreply.github.com>
2023-11-24 14:36:03 +0800

    滨崎荣一郎 feat：init ddd project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/ai/App.java
@@ -0,0 +1,16 @@
+package cn.bugstack.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
